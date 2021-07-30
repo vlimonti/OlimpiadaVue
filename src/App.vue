94dd40ab6f747ab6e9b0b2a427cf25464b3232b0
Victor Limonti <victor_limonti@hotmail.com>
2021-07-30 16:01:22 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="titulo">
+      <h1>Campeonato</h1>
+      <small>Objetivo: Chegar a 10 pontos!</small>
+    </div>
+    <div>
+      <hr>
+      <label>Equipe 1</label><app-contador></app-contador>
+      <hr>
+      <label>Equipe 2</label><app-contador></app-contador>
+      <hr>
+      <label>Equipe 3</label><app-contador></app-contador>
+      <hr>
+      <label>Equipe 4</label><app-contador></app-contador>
+      <hr>
+      <label>Equipe 5</label><app-contador></app-contador>
+      <hr>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  label {
+    font-weight: 600;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  }
+
+  h1 {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    margin: 0px 0px 0px 0px;
+  }
+
+  .titulo {
+    margin: 10px 0px 30px 0px;
+  }
 </style>
---
+++ src/Contador.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="contador">
+    <span>{{ contador }}</span>
+    <button @click="adicionar">+</button>
+    <button @click="subtrair">-</button>
+    <div v-if="result == true" 
+        :class="cls"
+        class="resultado">{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+let posicao = 0;
+
+export default {
+    data() {
+        return {
+        contador: 0,
+        result: false,
+        msg: '',
+        cls: ''
+        }
+    },
+    computed: {
+        hasResult() {
+            return this.contador == 10
+        }
+    },
+    methods: {
+        adicionar() {
+            if (this.result == false) {
+                this.contador++
+            }
+        },
+        subtrair() {
+            if (this.result == false) {
+                if (this.contador > 0) {
+                    this.contador--
+                }
+            }
+        }
+    },
+    watch: {
+        hasResult(value) {
+            if (value) 
+                posicao++
+                this.msg = posicao+"° Lugar"
+                this.result = true
+                if(posicao == 1)
+                    this.cls = 'ouro'
+                else if(posicao == 2)
+                    this.cls = 'prata'
+                else if (posicao == 3)
+                    this.cls = 'bronze'
+                else 
+                    this.cls = ''
+        }
+    }
+}
+</script>
+
+<style>
+    .contador span {
+    border-bottom: 1px solid #ccc;
+    height: 30px;
+    padding: 5px 25px;
+    }
+
+    .contador button {
+    height: 30px;
+    width: 30px;
+    border-radius: 15px;
+    background-color: coral;
+    color: white;
+    margin-left: 10px;
+    outline: none;
+    }
+
+    .resultado {
+        background-color: white;
+        color: black;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        width:20%;
+    }
+
+    .ouro {
+        background-color: rgb(243, 227, 6);
+        color: black;
+    }
+
+    .prata {
+        background-color: rgb(201, 201, 201);
+        color: black;
+    }
+
+    .bronze {
+        background-color: rgb(177, 134, 41);
+        color: black;
+    }
+</style>
