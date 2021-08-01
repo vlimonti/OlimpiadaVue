55b03068e50d6a3fe1834521ee1d1418795ce0cc
Victor Limonti <victor_limonti@hotmail.com>
2021-08-01 16:47:12 -0300

    Refatoração de código de ordenção

--- src/Contador.vue
+++ src/Contador.vue
@@ -56,27 +56,18 @@ export default {
     computed: {
         sortedTeams: function (){
             return this.teams.slice().sort(function(a, b){
-                if (a.medalhas.ouro > b.medalhas.ouro)
+                if (a.medalhas.ouro != b.medalhas.ouro)
+                    return b.medalhas.ouro - a.medalhas.ouro;
+                if (a.medalhas.prata != b.medalhas.prata)
+                    return b.medalhas.prata - a.medalhas.prata;
+                if (a.medalhas.bronze != b.medalhas.bronze)
+                    return b.medalhas.bronze - a.medalhas.bronze;
+                if (a.nome < b.nome)
                     return -1
-                if (a.medalhas.ouro < b.medalhas.ouro)
+                if (a.nome > b.nome)
                     return 1
                 else
-                    if (a.medalhas.prata > b.medalhas.prata)
-                        return -1
-                    if (a.medalhas.prata < b.medalhas.prata)
-                        return 1
-                    else
-                        if (a.medalhas.bronze > b.medalhas.bronze)
-                            return -1
-                        if (a.medalhas.bronze < b.medalhas.bronze)
-                            return 1
-                        else
-                            if (a.nome < b.nome)
-                                return -1
-                            if (a.nome > b.nome)
-                                return 1
-                            else
-                                return 0
+                    return 0
             });
         },
     }
